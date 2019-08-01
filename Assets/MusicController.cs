0869a8c9c20d3edee61778e6d34d66e60853d196
ThGnommy <th.brandoli@gmail.com>
2019-08-01 16:30:12 +0200

    added music and ambience

---
+++ Assets/MusicController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class MusicController : MonoBehaviour
+{
+    public AudioMixerSnapshot MainMenu;
+    public AudioMixerSnapshot Game;
+    public AudioMixerSnapshot Death;
+
+    private AudioSource audioSource;
+
+    public AudioClip[] track;
+
+
+}
